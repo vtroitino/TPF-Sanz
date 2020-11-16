af19bf209f3168ca17248d64e696fb23675aa95b
Valentin Troitiño <valiktroi16@gmail.com>
2020-11-16 03:55:50 -0300

    Borro archivos inescesarios

--- php/login.php
+++ php/login.php
@@ -46,7 +46,7 @@
 ?>
 <!DOCTYPE html>
 <head>
-    <title>Login</title>
+    <title>Frederic Moon - Login</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
     <link type="text/css" rel="stylesheet" href="../css/estilo.css">
--- sqlite/login.sql
+++ sqlite/login.sql
@@ -5,16 +5,3 @@ CREATE TABLE users(
    contraseña VARCHAR(200) NOT NULL,
    saldo INT NOT NULL
 );
-
-CREATE TABLE terrenos(
-   id INT PRIMARY KEY NOT NULL,
-   terreno VARCHAR(200) NOT NULL,
-   user_id INT
-);
-
-INSERT INTO terrenos(id, terreno) VALUES('1', 'Copernicus');
-INSERT INTO terrenos(id, terreno) VALUES('2', 'Kepler');
-INSERT INTO terrenos(id, terreno) VALUES('3', 'Langrenus');
-INSERT INTO terrenos(id, terreno) VALUES('4', 'Stevinus');
-INSERT INTO terrenos(id, terreno) VALUES('5', 'Oceanus Procellarum');
-INSERT INTO terrenos(id, terreno) VALUES('6', 'Eduardo Castex');
